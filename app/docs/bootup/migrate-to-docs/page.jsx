620bd0716113ab40d7aef62b84c9b29df8765daa
shreyvijayvargiya <shreyvijayvargiya26@gmail.com>
2025-11-18 16:16:37 +0530

    build error resolved

--- app/docs/bootup/migrate-to-docs/page.jsx
+++ app/docs/bootup/migrate-to-docs/page.jsx
@@ -1,11 +1,11 @@
-import Content from "../migrate-to-Docs.mdx";
+import Content from "../migrate-to-docs.mdx";
 import MDXContent from "../../../../components/MDXContent";
 import { navigationData } from "../../../../lib/navigation-data";
 
 export default function MigrateToDocsPage() {
 	return (
 		<MDXContent
-			currentRoute="/docs/bootup/migrate-to-Docs"
+			currentRoute="/docs/bootup/migrate-to-docs"
 			section={navigationData.docs}
 			baseRoute="/docs"
 		>
